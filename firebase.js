85f4e7b6a24e423a41de98b72581c4b7f0c8fcdb
Afnan Ahmed Crystal <90231930+afnancrystal@users.noreply.github.com>
2025-04-14 19:32:25 -0400

    Added recipe suggestion

--- firebase.js
+++ firebase.js
@@ -1,13 +1,32 @@
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAK0HvZfunecO2c7KckfttRo8e8U6RJy9U",
+//     authDomain: "inventory-a75dd.firebaseapp.com",
+//     projectId: "inventory-a75dd",
+//     storageBucket: "inventory-a75dd.firebasestorage.app",
+//     messagingSenderId: "724646748767",
+//     appId: "1:724646748767:web:466243489b1b4dfae031ea"
+//   };
+// const app = initializeApp(firebaseConfig);
+// const firestore = getFirestore(app);
+// export { firestore };
+
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+
+// web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAK0HvZfunecO2c7KckfttRo8e8U6RJy9U",
-    authDomain: "inventory-a75dd.firebaseapp.com",
-    projectId: "inventory-a75dd",
-    storageBucket: "inventory-a75dd.appspot.com",
-    messagingSenderId: "724646748767",
-    appId: "1:724646748767:web:466243489b1b4dfae031ea"
- };
+  apiKey: "AIzaSyAK0HvZfunecO2c7KckfttRo8e8U6RJy9U",
+  authDomain: "inventory-a75dd.firebaseapp.com",
+  projectId: "inventory-a75dd",
+  storageBucket: "inventory-a75dd.firebasestorage.app",
+  messagingSenderId: "724646748767",
+  appId: "1:724646748767:web:466243489b1b4dfae031ea"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 export { firestore };
+
